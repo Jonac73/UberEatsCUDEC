102785f56487e3e8d7931d1fb7cc5ed0e044de1f
Jonac73 <jc734090@gmail.com>
2026-06-15 20:27:22 -0600

    agregar platillo

---
+++ js/db.js
@@ -0,0 +1,31 @@
+db.collection("PLATILLOS").onSnapshot((coleccion) => {
+    coleccion.docChanges().forEach((registro) => {
+        if (registro.type === "added")
+        {
+            mostrarplatillo(registro.doc.data(), registro.doc.id);
+        }
+        if (registro.type === "modified")
+        {
+            actualizarplatillo(registro.doc.data(), registro.doc.id);
+        }
+    })});
+
+    const formularioAgregar = document.querySelector("form");
+  formularioAgregar.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const platilloNuevo = {
+      nombre: formularioAgregar.title.value,
+      ingredientes: formularioAgregar.ingredients.value,
+      costo: formularioAgregar.cost.value
+    }
+    db.collection("PLATILLOS").add(platilloNuevo)
+    .catch((error) => {
+        console.log(error);
+        alert("Error al agregar el platillo");
+    });
+    formularioAgregar.title.value = "";
+    formularioAgregar.ingredients.value = "";
+    formularioAgregar.cost.value = "";
+    alert("Platillo agregado exitosamente");
+  });
+
--- js/index.js
+++ js/index.js
@@ -6,3 +6,37 @@ document.addEventListener('DOMContentLoaded', function() {
   const forms = document.querySelectorAll('.side-form');
   M.Sidenav.init(forms, {edge: 'left'});
 });
+
+
+
+function mostrarplatillo(platillo, id)
+{
+  contenido = `
+  <div class='card-panel recipe white row' id='${id}'>
+    <div class='recipe-details'>
+      <div class='recipe-title'>
+        ${platillo.nombre}
+      </div>
+      <div class='recipe-ingredients'>
+        ${platillo.ingredientes}
+      </div>  
+      <div class='recipe-cost'>
+        $${platillo.costo}
+      </div>
+      <div class='recipe-delete'>
+        <i class='material-icons' data-id='${id}'>delete_outline</i>
+      </div>
+    </div>
+  </div>
+  `;
+  
+  document.querySelector(".recipes").innerHTML += contenido;
+
+  function actualizarplatillo(platillo, id)
+  {
+  let tarjeta = document.getElementById(`.recipe[data-id=${id}]`);
+  tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+  tarjeta.querySelector(".recipe-ingredients").innerHTML = platillo.ingredientes;
+  tarjeta.querySelector(".recipe-cost").innerHTML  = `$${platillo.costo}`;
+  }
+}
